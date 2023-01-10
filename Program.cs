74590d14edd3920e18d63dd19c19bfc316ee536d
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-01-10 16:48:51 -0300

    praticing while

--- Program.cs
+++ Program.cs
@@ -1,7 +1,36 @@
-﻿int numero = 5;
+﻿// While
+
+int num = 5;
+int contador = 0;
+
+while(contador <= 10)
+    {
+            Console.WriteLine($"{num} x {contador} =  {num * contador}");
+            contador ++;
+
+}
+
+
+
+
+/*int numero = 5;
 
 for (int contador = 0; contador <= 10; contador++)
 {
     Console.WriteLine($"{numero} x {contador} =  {numero * contador}");
 }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
